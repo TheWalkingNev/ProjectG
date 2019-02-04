41639b47b52e7b36493f820beac33909400a1fcd
TheWalkingNev <alexandre.gasior@gmail.com>
2019-02-04 21:26:01 +0100

    On test la nouvelle wii U avec ta maman

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+Gossip.destroy_all
+ActiveRecord::Base.connection.reset_pk_sequence!('users')
+ActiveRecord::Base.connection.reset_pk_sequence!('gossips')
+
+50.times do |i|
+	user = User.create!(
+    first_name: Faker::Superhero.prefix,
+    last_name: Faker::BackToTheFuture.character,
+    username: Faker::DragonBall.character,
+    email: Faker::Internet.email,
+    age: rand(12..120),
+    city: Faker::StarWars.planet,
+    description: Faker::StrangerThings.quote)
+  print '.'
+end
+puts '=== User DB created ==='
+
+50.times do |j|
+	gossip = Gossip.create!(
+    user_id: User.all.sample.id,
+    title: Faker::Pokemon.move,
+    content: Faker::StarWars.wookiee_sentence)
+	print '.'
+end
+puts '=== Gossip DB created ==='
