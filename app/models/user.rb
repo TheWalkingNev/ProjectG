638f9dd9c7239d61b884e589b171115b9e1abbf3
TheWalkingNev <alexandre.gasior@gmail.com>
2019-02-08 16:15:00 +0100

    Ben en fait il s'appelle Ted

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@ class User < ApplicationRecord
 
   has_many :gossips
   has_many :comments
+  has_many :sent_messages, foreign_key: 'sender_id', class_name: "PrivateMessage"
+  has_many :received_messages, foreign_key: 'recipient_id', class_name: "PrivateMessage"
 
   validates :first_name, presence: true
   validates :last_name, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_07_223819) do
+ActiveRecord::Schema.define(version: 2019_02_08_144759) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,16 @@ ActiveRecord::Schema.define(version: 2019_02_07_223819) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_gossips_on_user_id"
+  end
+
+  create_table "private_messages", force: :cascade do |t|
+    t.text "content"
+    t.bigint "recipient_id"
+    t.bigint "sender_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["recipient_id"], name: "index_private_messages_on_recipient_id"
+    t.index ["sender_id"], name: "index_private_messages_on_sender_id"
   end
 
   create_table "users", force: :cascade do |t|
